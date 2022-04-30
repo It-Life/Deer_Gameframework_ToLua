20b1d24ea2a1ee8ce41e03da2383f3d3d189f1f1
onion <652597991@qq.com>
2022-04-30 13:07:00 +0800

    自动设置导入图片sprite格式

---
+++ Assets/Editor/Lazy/AssetPostprocessor/AutoSetSpriteFormat.cs
@@ -0,0 +1,35 @@
+// ================================================
+//描 述:
+//作 者:杜鑫
+//创建时间:2022-04-30 10-15-58
+//修改作者:杜鑫
+//修改时间:2022-04-30 10-15-58
+//版 本:0.1 
+// ===============================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+/// <summary>
+/// Please modify the description.
+/// </summary>
+public class AutoSetSpriteFormat : AssetPostprocessor
+{
+        void OnPreprocessTexture()
+        {
+            //自动设置类型;
+            TextureImporter textureImporter = (TextureImporter)assetImporter;
+            string dirName = Path.GetDirectoryName(assetPath);
+            string atlasName = Path.GetFileNameWithoutExtension(assetPath);
+            string folderStr = Path.GetFileName(dirName);
+            if (assetPath.Contains("Assets/Deer/Asset/UI/UIArt/Texture"))
+            {
+                //textureImporter.textureType = TextureImporterType.Sprite;
+            }else if(assetPath.Contains("Assets/UITemp"))
+			{
+				textureImporter.textureType = TextureImporterType.Sprite;
+			}
+        }
+}
